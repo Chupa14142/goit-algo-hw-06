fb5b15041d081f16af98a55e6d878a7f2361b511
= <chupa14142@gmail.com>
2024-05-13 00:29:52 +0300

    add field.py

---
+++ field.py
@@ -0,0 +1,13 @@
+"""Field"""
+
+class Field:
+    """Field"""
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+if __name__ == "__main":
+    pass
